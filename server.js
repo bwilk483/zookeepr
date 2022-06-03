6c310e3557c8f15ff167b923c40602360c8f92cb
Bryan Wilkerson <bwilk483@gmail.com>
2022-06-03 11:16:04 -0500

    changed port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { animals } = require("./data/animals.json");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`API now on port 3001!`);
 });
 
