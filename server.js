addeedacfaf5ed842be1ec0bf85db61abf81aeed
Bryan Wilkerson <bwilk483@gmail.com>
2022-06-04 20:15:09 -0500

    model 3 done

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
-const { json } = require("body-parser");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//parse incoming strings or arrey dara
 app.use(express.urlencoded({ extended: true }));
-
-// parse incoming json data
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -98,13 +95,28 @@ app.post("/api/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send("The animal is not properly formatted.");
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public.zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
